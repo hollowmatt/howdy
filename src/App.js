c5d31acd6542eaa2b23328e3b5c8cd1e82ba9cc7
Matt Holloway <hollowmatt@gmail.com>
2019-02-18 21:02:22 -0500

    move files around

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './components/css/app.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Quiz from './components/quiz.component';
 import Index from './components/index.component';
@@ -11,20 +11,6 @@ class App extends Component {
     return (
       <Router>
         <div className="container-fluid">
-          <ul className="nav justify-content-center">
-            <li className="nav-item">
-              <Link to={'/howdy'} className="nav-link active" >How is your day?</Link>
-            </li>
-            <li className="nav-item">
-              <Link to={'#'} className="nav-link">Meeting Tracker</Link>
-            </li>
-            <li className="nav-item">
-              <Link to={'#'} className="nav-link">Time Tracker</Link>
-            </li>
-            <li className="nav-item">
-              <Link to={'#'} className="nav-link disabled">Login</Link>
-            </li>
-          </ul>  
           <Switch>
             <Route exact path='/' component={ Index } />
             <Route exact path='/howdy' component={ Quiz } /> 
