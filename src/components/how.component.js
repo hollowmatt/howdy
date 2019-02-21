a425046f83cf5c4e3b556f22fdcef08c703f1d47
Matt Holloway <hollowmatt@gmail.com>
2019-02-20 21:36:19 -0500

    fixing the how component display

--- src/components/how.component.js
+++ src/components/how.component.js
@@ -6,20 +6,31 @@ class How extends Component {
   render() {
     return (
       <div className={this.props.data.style}>
-        <h1 className="display-4 center">{this.props.data.title}</h1>
-        <hr className="my-4 center"/>
-        <p className="center">
-          {this.props.data.images.map(function(image, index) {
-          	return( 
-              <ul className="image-box">
-                <li>
-                  <img className="how-image" src={image.imageURL} key={index} height="200" width="200"/>
-                  <p>{image.imageText}</p>
-                </li>
-              </ul>
-            )
-          })}
-        </p>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">
+              <h1 className="display-4 center">{this.props.data.title}</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm">
+              <hr className="my-4 center"/>
+            </div>
+          </div>
+          <div className="row">
+            {this.props.data.images.map(function(image, index) {
+            	return( 
+                <div className="col-sm">
+                  <div className="image-box" key={index} >
+                    <img className="how-image" src={image.imageURL} height="200" width="200"/>
+                    <p>{image.imageText}</p>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+          <div className="float-clear"></div>
+        </div>
       </div>
     );
   }
