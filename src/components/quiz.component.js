293259fb9e43c850f12592c0c91f89c99840154f
Matt Holloway <hollowmatt@gmail.com>
2019-02-25 21:32:07 -0500

    Setup for mobile rendering a different template than desktop

--- src/components/quiz.component.js
+++ src/components/quiz.component.js
@@ -32,37 +32,68 @@ function log(value) {
 };
 
 export default class Quiz extends Component {
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  // make sure to remove the listener
+  // when the component is not mounted anymore
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
   render() {
-    return (
-      <div className="index">
-        <Header />
-        <div className="day-box">
-          <div className="row">
-            <div className="col-12" align="center">
-              <h2>How bad was your day?</h2>
+    const { width } = this.state;
+    const isMobile = width <= 500;
+    // the rest is the same...
+
+    if (isMobile) {
+      return (
+        <p> This is a mobile view</p>
+      );
+    } else {
+      return (
+        <div className="index">
+          <Header />
+          <div className="day-box">
+            <div className="row">
+              <div className="col-12" align="center">
+                <h2>How bad was your day?</h2>
+              </div>
             </div>
+            <form>
+              <div className="form-group row">
+                <div className="col-12" align="center">
+                  <Slider dots min={1} max={7} marks={marks} step={1} onChange={log} defaultValue={4} />
+                </div>
+              </div>
+              <div className="form-group row">
+                <div className="col-12" align="center">
+                  <input type="checkbox" name="escalation" value="escalation"/>
+                  <label className="label-text">Check if there was an escalation today</label>
+                </div>
+              </div>
+              <div className="form-group row">
+                <div className="col-12" align="right">
+                  <input type="submit" value="SUBMIT" className="btn btn-large orange-background button-text day-btn"/>
+                </div> 
+              </div>
+            </form>
           </div>
-          <form>
-            <div className="form-group row">
-              <div className="col-12" align="center">
-                <Slider dots min={1} max={7} marks={marks} step={1} onChange={log} defaultValue={4} />
-              </div>
-            </div>
-            <div className="form-group row">
-              <div className="col-12" align="center">
-                <input type="checkbox" name="escalation" value="escalation"/>
-                <label className="label-text">Check if there was an escalation today</label>
-              </div>
-            </div>
-            <div className="form-group row">
-              <div className="col-12" align="right">
-                <input type="submit" value="SUBMIT" className="btn btn-large orange-background button-text day-btn"/>
-              </div> 
-            </div>
-          </form>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    );
+      );
+    } 
   }
 }
+
